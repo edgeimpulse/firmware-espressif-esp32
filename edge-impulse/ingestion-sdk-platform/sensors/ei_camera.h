7ea30f8942fc74e3a9d06d03af78bac3d4f2e568
icing@tomato:~$ <peter@icing.fun>
2022-07-21 08:35:17 +0800

    Update ei_camera.h
    SIDO and VSYNC changed

--- edge-impulse/ingestion-sdk-platform/sensors/ei_camera.h
+++ edge-impulse/ingestion-sdk-platform/sensors/ei_camera.h
@@ -91,7 +91,7 @@
   #define PWDN_GPIO_NUM     -1
   #define RESET_GPIO_NUM    15
   #define XCLK_GPIO_NUM     27
-  #define SIOD_GPIO_NUM     25
+  #define SIOD_GPIO_NUM     22
   #define SIOC_GPIO_NUM     23
   #define Y9_GPIO_NUM       19
   #define Y8_GPIO_NUM       36
@@ -101,7 +101,7 @@
   #define Y4_GPIO_NUM       34
   #define Y3_GPIO_NUM       35
   #define Y2_GPIO_NUM       32
-  #define VSYNC_GPIO_NUM    22
+  #define VSYNC_GPIO_NUM    25
   #define HREF_GPIO_NUM     26
   #define PCLK_GPIO_NUM     21
   // M5 Stack status/illumination LED details unknown/unclear
